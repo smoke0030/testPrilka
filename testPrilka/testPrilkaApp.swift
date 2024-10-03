142cf19264fd1683b488e3d1d4fadbae6c94c70d
smoke0030 <ft.priest@gmail.com>
2024-10-03 15:47:27 +0300

    Initial Commit

---
+++ testPrilka/testPrilkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  testPrilkaApp.swift
+//  testPrilka
+//
+//  Created by Сергей on 03.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct testPrilkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
